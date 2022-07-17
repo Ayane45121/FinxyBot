ca4c3a19ab2ab6852d9470b46d0a5b4d34725d18
Ayane45121 <108357375+Ayane45121@users.noreply.github.com>
2022-07-17 17:13:06 +0700

    Update config.js

--- config.js
+++ config.js
@@ -2,9 +2,9 @@ let fs = require('fs')
 let chalk = require('chalk')
 
 global.owner = [
-  ['6288227758096'],
-  ['6288227758096'],
-  ['6288227758096', 'Owner', true]
+  ['6287734910547'],
+  ['6287734910547'],
+  ['6287734910547', 'VinzKecot:v', true]
   // [number, dia creator/owner?, dia developer?]
 ] // Put your number here
 global.mods = [] // Want some help?
@@ -40,8 +40,8 @@ process.env['NODE_TLS_REJECT_UNAUTHORIZED'] = 0
 const spack = fs.readFileSync("lib/exif.json")
 const stickerpack = JSON.parse(spack)
 if (stickerpack.spackname == '') {
-  var sticker_name = '@FazhBoT'
-  var sticker_author = 'FazhBoT'
+  var sticker_name = '@FinxyBoT'
+  var sticker_author = 'FinxyBoT'
 } else {
   var sticker_name = stickerpack.spackname
   var sticker_author = stickerpack.sauthor
@@ -58,7 +58,7 @@ fs.watchFile(file_exif, () => {
 // Sticker WM
 global.packname = sticker_name
 global.author = sticker_author
-global.wm = '© Fachri'
+global.wm = '© Finxy'
 
 global.multiplier = 1000 // The higher, The harder levelup
 
